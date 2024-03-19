183565748c001503e3ab31bb371595868cfd44e4
Sydney Abam <106718254+saabam23@users.noreply.github.com>
2024-03-19 00:01:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Auto Typing text 
+let typed = new Typed("#autoType", {
+    strings: ["ABOUT ME", "ABOUT ME", "ABOUT ME"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 100,
+    loop: true,
+    loopCount: Infinity
+})
+
+
+
